61a1b0a06bfd8520aa94d5b98bbdd819ea3f2ece
Aishanya Mishra <154995879+aishanya20@users.noreply.github.com>
2025-08-05 08:09:52 +0530

    Update: [new file serialzers.py and used get method and Serializer]

--- api/views.py
+++ api/views.py
@@ -1,11 +1,23 @@
-from django.shortcuts import render
-from django.http import JsonResponse
+# from django.shortcuts import render
+# from django.http import JsonResponse
+from students.models import Students
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
 
-# Create your views here.
+
+@api_view(['Get'])
 def studentView(request):
-    students={
-        'id':1,
-        'name':'XYZ',
-        'class': 'Computer Science'
-    }
-    return JsonResponse(students)
+    # students=Students.objects.all() #this brings the data of students model into students
+    # student_list=list(students.values()) #converting query sets into list
+    # return JsonResponse(student_list, safe=False) #safe parameter is for data is other than dictionaries
+    
+    if request.method =='GET':
+        #Get all the data from the Student table
+        Student = Students.objects.all()
+        serializer = StudentSerializer(Student,many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    
+    
